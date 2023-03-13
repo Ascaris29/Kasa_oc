d5e69eba5fec60b8b3e7935fe714642e2efaa9f4
Ascaris29 <solenelema2009@hotmail.com>
2023-03-14 00:38:42 +0100

    mise en place du layout et css

--- kasa_project/src/App.js
+++ kasa_project/src/App.js
@@ -1,7 +1,15 @@
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import style from "./App.module.scss";
+import Homepage from "./pages/homepage/Homepage";
 
 function App() {
   return (
-    <div></div>
+      <div className={`${style.app} d-flex flex-column`}>
+        <Header />
+        <Homepage />
+        <Footer />
+      </div>
   );
 }
 
