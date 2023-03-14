921f24f71785aee7653afaa4600abba6c31265d7
Ascaris29 <solenelema2009@hotmail.com>
2023-03-14 16:19:19 +0100

    progression responsive

--- kasa_project/src/pages/homepage/Homepage.js
+++ kasa_project/src/pages/homepage/Homepage.js
@@ -1,5 +1,6 @@
 import style from "./Homepage.module.scss";
 import image from "../../assets/images/background_homepage.png";
+import Card from "./components/Card";
 
 export default function Homepage(){
     return (
@@ -7,6 +8,9 @@ export default function Homepage(){
             <div className={`${style.homepageImg}`}>
                 <img src={image} alt="" />
                 <p className={`${style.homepageTxtImg}`}>Chez vous, partout et ailleurs</p>
+            </div>
+            <div className={`${style.homepageGallery} m-t-30`}>
+                <Card />
             </div>
         </main>
     )
