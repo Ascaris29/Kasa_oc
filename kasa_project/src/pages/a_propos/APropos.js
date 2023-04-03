0d24edc7a8aa63b1a227e0f795b9d565efd1e0fa
Ascaris29 <solenelema2009@hotmail.com>
2023-04-03 09:41:13 +0200

    correction

--- kasa_project/src/pages/a_propos/APropos.js
+++ kasa_project/src/pages/a_propos/APropos.js
@@ -9,10 +9,10 @@ export default function APropos(){
                     <img src={image} alt="photographie d'une montagne et d'un lac"/>
                 </div>
                 <div className={`${style.aproposCards}`}>
-                    <CardDescription className="" title="fiabilité" text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." key={"1"}/>
-                    <CardDescription className="" title="Respect" text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoires ou de perturbation du voisinage entrainera une exclusion de notre plateforme." key={"2"}/>
-                    <CardDescription className="" title="Service" text="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question." key={"3"}/>
-                    <CardDescription className="" title="Responsabilité" text="La sécurité est la propriété de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standars sont bien respectés. Nous organisons également des ateliers sur la sécurité domestiques pour nos hôtes." key={"4"}/> 
+                    <CardDescription title="Fiabilité" text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." key={"1"}/>
+                    <CardDescription title="Respect" text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoires ou de perturbation du voisinage entrainera une exclusion de notre plateforme." key={"2"}/>
+                    <CardDescription title="Service" text="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question." key={"3"}/>
+                    <CardDescription title="Responsabilité" text="La sécurité est la propriété de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standars sont bien respectés. Nous organisons également des ateliers sur la sécurité domestiques pour nos hôtes." key={"4"}/> 
                 </div>
             </div>
     )
