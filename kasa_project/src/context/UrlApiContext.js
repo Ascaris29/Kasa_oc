1da452d4a4f0794d0a40c8e3aec166295c5ec78d
Ascaris29 <solenelema2009@hotmail.com>
2023-04-03 10:50:33 +0200

    essai

--- kasa_project/src/context/UrlApiContext.js
+++ kasa_project/src/context/UrlApiContext.js
@@ -1,3 +1,3 @@
 import { createContext } from "react";
 
-export const UrlApiContext = createContext("/https://raw.githubusercontent.com/Ascaris29/Kasa_oc/main/kasa_project/public/datas.json");
+export const UrlApiContext = createContext("https://raw.githubusercontent.com/Ascaris29/Kasa_oc/main/kasa_project/public/datas.json");
