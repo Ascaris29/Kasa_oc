3e8466942350017de9d2f8519f83681a363d4855
Ascaris29 <solenelema2009@hotmail.com>
2023-04-06 08:32:18 +0200

    dernier commit

--- kasa_project/src/pages/logement/components/caroussel/Caroussel.js
+++ kasa_project/src/pages/logement/components/caroussel/Caroussel.js
@@ -8,31 +8,36 @@ export default function Caroussel({img}){
     const arrayImage = img;
 
     const [index, setIndex] = useState(0);
+    const [loading, setLoading] = useState(false);
     const arraySize = arrayImage.length;
     const indexShow = index + 1 ;
 
     //console.log(arrayImage);
     function handleClickCarousselPrev(){
         if(index > 0 && index < arrayImage.length){
+            setLoading(true)
             let i = index - 1;
             setIndex(i);
         }else{
+            setLoading(true)
             setIndex(arrayImage.length -1);
         }
     }
 
     function handleClickCarousselAfter(){
         if(index < arrayImage.length -1 ){
+            setLoading(true)
             let i = index + 1;
             setIndex(i);
         }else{
+            setLoading(true)
             setIndex(0);
         }   
     }
     return (
         <>
             { arrayImage.map((item, i) => i === index && ( 
-                <img src={item} alt="" className={`${style.carousselImage}`} key={item}/> 
+                <img src={item} alt="" className={`${style.carousselImage} ${loading ? "loader" : null}`} key={item}/> 
             )
             )}
             <p className={`${style.indexShow}`}>{`${indexShow} / ${arraySize}`}</p>
